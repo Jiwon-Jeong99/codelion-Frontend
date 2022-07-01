7d4169be15331b7764bd0617f5658a001fe487a8
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-02 04:13:52 +0900

    드디어 저장

--- 실습/project/src/Slogun.js
+++ 실습/project/src/Slogun.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { SlogunBig, SlogunSection, SlogunSmall } from "./styledComponent";
 
 const Slogun = () => {
   return (
     <SlogunSection>
-              <SlogunBig>Hack Your Life</SlogunBig>
-              <SlogunSmall>내 아이디어를 내 손으로 실현한다</SlogunSmall>
-            </SlogunSection>
-  )
-}
+      <SlogunBig>Hack Your Life</SlogunBig>
+      <SlogunSmall>내 아이디어를 내 손으로 실현한다</SlogunSmall>
+    </SlogunSection>
+  );
+};
 
-export default Slogun
+export default Slogun;
